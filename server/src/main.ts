5111f05d4fe6369137e1a3dd053d46feb693001d
ldamoredev <lauti.damore@gmail.com>
2026-03-26 00:47:00 -0300

    deploying

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { Core } from './modules/Core';
 import { App } from './App';
 import { NodeRuntimeLifecycle } from './runtime/NodeRuntimeLifecycle';
 
-const PORT = Number(process.env.PORT) || 4001;
+const PORT = Number(process.env.PORT) || 4000;
 const HOST = '0.0.0.0';
 
 async function main() {
